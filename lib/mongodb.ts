6ddb33bf55642246bf92a63c585806448a7ac9e8
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2023-03-21 11:41:51 +0100

    fix list asset

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "development") {
     client = new MongoClient(uri, options);
     globalWithMongo._mongoClientPromise = client.connect();
   }
-  clientPromise = globalWithMongo._mongoClientPromise;
+  clientPromise = globalWithMongo._mongoClientPromise!;
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options);
